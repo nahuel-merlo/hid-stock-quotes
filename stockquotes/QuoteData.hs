1700ef63309d4aa28dce5a47986a1e52554ecbf4
nahuepera5 <n.peraza04@gmail.com>
2026-04-15 20:25:48 -0300

    (feat): represent quote data

--- stockquotes/QuoteData.hs
+++ stockquotes/QuoteData.hs
@@ -1,1 +1,31 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
 module QuoteData where
+
+import Data.Time (Day, parseTimeM, defaultTimeLocale)
+import Data.ByteString.Char8 (unpack)
+import GHC.Generics (Generic)
+import Data.Csv (FromNamedRecord, FromField (..))
+
+data QuoteData = QuoteData {
+    day    :: Day, 
+    volume :: Int,
+    open   :: Double,
+    close  :: Double,
+    high   :: Double,
+    low    ::  Double
+} deriving (Generic, FromNamedRecord)
+
+data QField = Open | Close | High | Low | Volume
+    deriving (Eq, Ord, Show, Enum, Bounded)
+
+instance FromField Day where
+    parseField = parseTimeM True defaultTimeLocale "%y%m-%d" . unpack
+
+field2fun :: QField -> QuoteData -> Double
+field2fun Open   = open
+field2fun Close  = close
+field2fun High   = high
+field2fun Low    = low
+field2fun Volume = fromIntegral . volume
