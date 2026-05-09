2c75c2cc9a39ce4e27d198a80616763b850915e5
nahuepera5 <n.peraza04@gmail.com>
2026-05-09 01:56:19 -0300

    (feat): add Html report

--- stockquotes/HtmlReport.hs
+++ stockquotes/HtmlReport.hs
@@ -1,1 +1,60 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module HtmlReport where
+
+import Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes (src)
+import Text.Blaze.Html.Renderer.Utf8 (renderHtml)
+import Text.Blaze.Colonnade
+import Colonnade
+import Fmt (pretty, Buildable)
+import Data.ByteString.Lazy (ByteString)
+import Control.Monad (unless)
+import Data.Foldable (traverse_)
+
+import StatReport
+import QuoteData
+import QuoteIO
+
+viaFmt :: Buildable a => a -> Html
+viaFmt = text . pretty
+
+colStats :: Colonnade Headed StatEntry Html 
+colStats = mconcat 
+    [ headed "Quote Field" (i . string . show . qfield)
+    , headed "Mean" (viaFmt . meanVal)
+    , headed "Min" (viaFmt . minVal)
+    , headed "Max" (viaFmt . maxVal)
+    , headed "Days between Min/Max" (viaFmt . daysBetweenMinMax)
+    ]
+
+colData :: Colonnade Headed QuoteData Html
+colData = mconcat
+    [ headed "Day" (viaFmt . day)
+    , headed "Open" (viaFmt . showPrice . open)
+    , headed "Close" (viaFmt . showPrice . close)
+    , headed "High" (viaFmt . showPrice . high)
+    , headed "Low" (viaFmt . showPrice . low)
+    , headed "Volume" (viaFmt . volume)
+    ]
+
+htmlReport :: (Functor t, Foldable t) => String -> t QuoteData -> [StatEntry] -> [FilePath] -> ByteString
+htmlReport docTitle quotes statEntries images = renderHtml $ docTypeHtml $ do
+    H.head $ do
+        title $ string docTitle
+        style tabStyle
+    body $ do
+        unless (null images) $ do
+            h1 "Carts"
+            traverse_ ((img!).src.toValue) images
+
+        h1 "Statistics Report"
+        encodeHtmlTable mempty colStats statEntries
+
+        h1 "Stock Quotes Data"
+        encodeHtmlTable mempty colData quotes
+    where
+        tabStyle = "table {border-collapse: collapse}" <>
+                   "td, th {border: 1px solid black; padding: 5px}"
+
+printHtml = readQuotes "stockquotes/data/quotes.csv" >>= printCompactHtml . (encodeHtmlTable mempty colStats) . statInfo 
